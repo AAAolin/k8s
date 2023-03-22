d2af9179905a1f58b9ff4e62ad8fbf8e3f00e505
AAAolin <114200136+AAAolin@users.noreply.github.com>
2023-03-22 21:02:50 +0800

    Update config.go

--- config/config.go
+++ config/config.go
@@ -3,25 +3,8 @@ package config
 import "time"
 
 const (
-	ListenAddr = "0.0.0.0:9091"
-	//KubeConfig = "D:\\GoProject\\k8s\\config\\config"
-	KubeConfig = "/root/.kube/config"
-	LogsLimit  = 2000
-
 	Username = "admin"
 	Password = "123456"
-
-	// 数据库配置
-	DBUser     = "root"
-	DBPassword = "123456"
-	DBType     = "mysql"
-	DBIP       = "192.168.1.16"
-	DBPort     = 3306
-	DBName     = "k8s"
-	// 连接池配置
-	MaxIdleConns = 10               //最大空闲连接
-	MaxOpenConns = 100              //最大连接数
-	MaxLifeTime  = 30 * time.Second //最大生存时间
 )
 
 type Student struct {
